70cb3bf81608bfc95252c05c036487e3177b75b7
Cristiano <neivacristiano@yahoo.com.br>
2021-11-20 17:05:43 -0300

    feat: add js file

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,24 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+function setExpandAction() {
+  const elements = document.querySelectorAll(".botao-expandir-retrair");
+
+  elements.forEach((e) => {
+    e.addEventListener("click", handleExpand);
+  });
+
+  return elements.length;
+}
+
+function handleExpand(e) {
+  const pElement = e.currentTarget.parentElement;
+
+  if (pElement.className === "expandido") {
+    e.currentTarget.innerHTML = "+";
+    return (pElement.className = undefined);
+  }
+
+  e.currentTarget.innerHTML = "-";
+  pElement.className = "expandido";
+}
